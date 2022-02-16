e784a95e79caad203bf03a5f64c4948ea659eb88
Jérôme Richard <jerrylejer23@yahoo.fr>
2022-02-16 19:38:37 +0100

    Page Home structure ok - Navigation ok

---
+++ src/App/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Nav from '../components/Nav/Nav';
+import Home from '../pages/Home/Home';
+import Competences from '../pages/Competences/Competences';
+import Creations from '../pages/Creations/Creations';
+import Viepro from '../pages/Viepro/Viepro';
+import Contacts from '../pages/Contacts/Contacts';
+import Page404 from '../pages/Page404/Page404';
+import Footer from '../components/Footer/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Nav /> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Competences" element={<Competences />} />
+          <Route path="/Creations" element={<Creations />} />
+          <Route path="/ViePro" element={<Viepro />} />
+          <Route path="/Contacts" element={<Contacts />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
